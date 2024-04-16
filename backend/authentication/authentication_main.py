16c88a7e4da0756318e2d2bf4bd88b4ffb1f9115
Piotr Szmurlo <pszmurlo@box.com>
2024-04-16 22:55:03 +0200

    decore/verify user info

--- backend/authentication/authentication_main.py
+++ backend/authentication/authentication_main.py
@@ -3,9 +3,13 @@ import json
 from fastapi import FastAPI
 
 import google_auth_oauthlib.flow
+import google_auth_oauthlib.interactive
+from google.oauth2 import id_token
+from google.auth.transport import requests
 from fastapi import Request
 from starlette.responses import RedirectResponse
 import os
+from google.auth import jwt
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 auth_app = FastAPI(debug=True)
@@ -15,6 +19,13 @@ flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
     'authentication/client_secret.json',
     scopes=['openid', 'https://www.googleapis.com/auth/userinfo.profile'])
 flow.redirect_uri = 'http://localhost:8000/auth/callback'
+
+CLIENT_ID = None
+
+
+def verify_token(token):
+    info = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+    return info
 
 
 @auth_app.get("/hello")
@@ -33,5 +44,6 @@ async def login():
 
 @auth_app.get("/callback")
 async def callback(request: Request):
-    flow.fetch_token(authorization_response=str(request.url))
-    return json.loads(flow.credentials.to_json())
+    token_dict = flow.fetch_token(authorization_response=str(request.url))
+    info = verify_token(token_dict["id_token"])
+    return info
