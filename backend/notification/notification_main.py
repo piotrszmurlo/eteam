d9e0519cbd372a6e255b0f666febd8f8eed9e0cc
itsMe <61231501+hubert-groen@users.noreply.github.com>
2024-04-27 18:52:52 +0200

    added except Integrity Error

--- backend/notification/notification_main.py
+++ backend/notification/notification_main.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Depends, HTTPException
-
+from sqlalchemy.exc import IntegrityError
 from common.dependencies import verify_token
 from notification.models import UserIdResponse, UserModel, UserEmailInput
 from notification.repository import NotificationRepository
@@ -21,7 +21,7 @@ async def add_user(user_email: UserEmailInput, token: Annotated[str, Depends(ver
     user = UserModel(user_id=token["sub"], user_name=token["given_name"], user_email=user_email.user_email)
     try:
         user_id = notification_repo.insert_user(user)
-    except:
+    except IntegrityError:
         existing_user_name = token["given_name"]
         raise HTTPException(status_code=400, detail=f"User {existing_user_name} already exists!")
     return UserIdResponse(user_id=user_id)
@@ -32,6 +32,6 @@ async def get_user_email(token: Annotated[str, Depends(verify_token)]) -> UserMo
     user_id=token["sub"]
     try:
         email = notification_repo.get_user(user_id=user_id)
-    except:
+    except IntegrityError:
         raise HTTPException(status_code=400, detail=f"User does not exist!")
     return email
--- backend/storage/storage_main.py
+++ backend/storage/storage_main.py
@@ -2,6 +2,7 @@ import uuid
 
 from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
 from common.dependencies import verify_token
 from storage.models import UserIdResponse, UserModel, FileModel, FileIdResponse, FileInsertModel, FileRenameModel, FileDeleteModel
 from storage.repository import StorageRepository
@@ -21,7 +22,7 @@ async def add_user(token: Annotated[str, Depends(verify_token)]) -> UserIdRespon
     user = UserModel(user_id=token["sub"], user_name=token["given_name"])
     try:
         user_id = storage_repo.insert_user(user)
-    except:
+    except IntegrityError:
         existing_user_name = token["given_name"]
         raise HTTPException(status_code=400, detail=f"User {existing_user_name} already exists!")
     return UserIdResponse(user_id=user_id)
@@ -34,7 +35,7 @@ async def get_user(token: Annotated[str, Depends(verify_token)]) -> UserModel:
     # user_id += "1"
     try:
         user = storage_repo.get_user(user_id=user_id)
-    except:
+    except IntegrityError:
         raise HTTPException(status_code=400, detail=f"User does not exist!")
     return user
 
@@ -45,7 +46,7 @@ async def add_file(file_input: FileInsertModel, token: Annotated[str, Depends(ve
     file = FileModel(user_id=token["sub"], file_name=file_input.file_name)
     try:
         file_id = storage_repo.insert_file(file)
-    except:
+    except IntegrityError:
         file_name = file_input.file_name
         raise HTTPException(status_code=400, detail=f"File {file_name} already exists!")
     return FileIdResponse(file_id=file_id)
@@ -58,7 +59,7 @@ async def get_files_for_user(token: Annotated[str, Depends(verify_token)]) -> li
     # user_id += "1"
     try:
         files = storage_repo.get_files(user_id=user_id)
-    except:
+    except IntegrityError:
         raise HTTPException(status_code=400, detail=f"User does not exist!")
     return files
 
@@ -68,7 +69,7 @@ async def rename_file(file_id: uuid.UUID, file_rename: FileRenameModel, token: A
     storage_repo = StorageRepository()
     try:
         updated_file_id = storage_repo.rename_file(file_id=file_id, new_file=file_rename)
-    except:
+    except IntegrityError:
         raise HTTPException(status_code=400, detail=f"File does not exist!")
     return FileIdResponse(file_id=updated_file_id)
 
@@ -78,6 +79,6 @@ async def delete_file(file_delete: FileDeleteModel, token: Annotated[str, Depend
     storage_repo = StorageRepository()
     try:
         deleted_file_id = storage_repo.delete_file(file_id=file_delete.file_id)
-    except:
+    except IntegrityError:
         raise HTTPException(status_code=400, detail=f"File does not exist!")
     return FileIdResponse(file_id=deleted_file_id)
